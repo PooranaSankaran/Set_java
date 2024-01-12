36234b5dedc25d2b973daf4675cfd8a08f5d1a56
PooranaSankaran <99974111+PooranaSankaran@users.noreply.github.com>
2024-01-13 03:12:30 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 package org.studyeasy;
 
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> set = new TreeSet<>();
-        for (int i = 30; i >=0 ; i--) {
-            set.add("A"+i);
-        }
-        for (String x:set ) {
-            System.out.println(x);
-        }
+        Set<String> set = new HashSet<>();
+        set.add("Chaand");
+        set.add("John");
+        set.add("Aafiya");
+        set.add("Mike");
+        set.add("Mia");
+        set.add("Chaand");
 
+        //System.out.println(set);
+
+        for (String name:set ) {
+            System.out.println(name);
+
+        }
+        System.out.println(set.contains("chaand"));
 
     }
 }
